0cfceb91074fba9014cfc007e1585542753a10ba
user8595 <muhikhsansyakir@gmail.com>
2025-10-12 17:38:30 +0800

    add game icon

--- conf.lua
+++ conf.lua
@@ -1,7 +1,7 @@
 function love.conf(t)
     t.identity = "time"
     t.window.title = "time"
-    -- t.window.icon = ""
+    t.window.icon = "/assets/icon.png"
     t.window.width = 220
     t.window.height = 280
     t.version = "11.5"
